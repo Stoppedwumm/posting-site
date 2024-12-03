f5c8a5347e447ed2e35a3f0db090b6fbefd2bdb7
stoppedwumm <mobileboxnews@icloud.com>
2024-12-03 18:43:40 +0100

    Add flags secret

--- src/comps/flags.js
+++ src/comps/flags.js
@@ -2,6 +2,6 @@ import { encrypt } from '@vercel/flags';
 import { FlagValues } from '@vercel/flags/react';
  
 export async function ConfidentialFlagValues({ values }) {
-  const encryptedFlagValues = await encrypt(values);
+  const encryptedFlagValues = await encrypt(values, process.env.FLAGS_SECRET);
   return <FlagValues values={encryptedFlagValues} />;
 }
