ac0e3ce2a1954477ee2958c7afa21178ccb2724f
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-12-04 09:25:01 +0100

    Add firebase

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
 import "@/custom.css";
 import { Suspense } from 'react'
+import fb from "@/server/firebase"
 /**
  * The root layout component.
  *
@@ -17,6 +18,14 @@ export const metadata = {
   description: "random post page (rpp)",
 };
 
+export const viewport = {
+
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-bs-theme="dark">
@@ -25,13 +34,13 @@ export default function RootLayout({ children }) {
         <header className="d-flex p-2 flex-row mb-3 align-items-baseline">
           <h1><Link href="/" className="white">random post page (rpp)</Link></h1>
           <div style={{ marginLeft: "auto" }}>
-          <Link href="/newPost" className="btn btn-primary" style={{ marginLeft: "100px" }}>new post</Link>
-          <Link href="/filter" className="btn btn-primary" style={{ marginLeft: "auto" }}>filter</Link>
+            <Link href="/newPost" className="btn btn-primary" style={{ marginLeft: "100px" }}>new post</Link>
+            <Link href="/filter" className="btn btn-primary" style={{ marginLeft: "auto" }}>filter</Link>
           </div>
-          
+
         </header>
         <Suspense fallback={<div>Loading...</div>}>
-        {children}
+          {children}
         </Suspense>
       </body>
     </html>
