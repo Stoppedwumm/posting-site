9304c3733252984c0df7b47e36ec35fad00b1052
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-12-04 08:29:15 +0100

    Remove flags

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,9 @@
 import Post from "c/post";
 import {getFlagsSecret, getPosts} from "@/server/processor"
 import { use, useEffect, useState } from "react";
-import { newFeature } from "@/flags";
 
 export default function Home() {
   const [p, setPosts] = useState([])
-  const [newFeatureEnabled, setNewFeatureEnabled] = useState(false)
   useEffect(() => {
     async function exec() {
       let posts = await getPosts()
@@ -17,16 +15,9 @@ export default function Home() {
     }
     exec()
   }, [])
-  useEffect(() => {
-    async function exec() {
-      setNewFeatureEnabled(await newFeature())
-    }
-    exec()
-  })
   return (
     <>
     <main>
-      {newFeatureEnabled ? <p>new feature enabled</p> : undefined}
       <h1>Top 10 latest posts:</h1>
       {/* Only load 5 posts at a time */}
       {p.length > 0 ? p.map((post) => (
--- src/flags.js
+++
@@ -1,8 +0,0 @@
-import {unstable_flag as flag} from '@vercel/flags/next'
-
-export const newFeature = flag({
-    key: 'newFeature',
-    description: 'Controls whether the new feature is visible',
-    decide: async () => false,
-    defaultValue: false
-})
